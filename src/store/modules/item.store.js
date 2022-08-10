30cd7506ed42bae096015aef4965b6fa26db82db
Guy Kadosh <guyk50@gmail.com>
2022-08-10 13:37:13 +0300

    deleted not needes services

--- src/store/modules/item.store.js
+++ src/store/modules/item.store.js
@@ -1,63 +1,65 @@
-import { itemService } from '../../services/item.service'
+import { itemService } from '../../services/comment.service'
 import { socketService } from '../../services/socket.service'
 
 export const itemStore = {
-    state: {
-        items: [],
+  state: {
+    items: [],
+  },
+  getters: {
+    items({ items }) {
+      return items
     },
-    getters: {
-        items({ items }) { return items },
+  },
+  mutations: {
+    setItems(state, { items }) {
+      state.items = items
     },
-    mutations: {
-        setItems(state, { items }) {
-            state.items = items
-        },
-        updateItem(state, { savedItem }) {
-            const idx = state.items.findIndex(i => i._id === savedItem._id)
-            console.log('idx', idx)
-            if (idx < 0) state.items.push(savedItem)
-            else state.items[idx] = savedItem
-        },
-        removeItem(state, { itemId }) {
-            state.items = state.items.filter(item => item._id !== itemId)
-        },
+    updateItem(state, { savedItem }) {
+      const idx = state.items.findIndex(i => i._id === savedItem._id)
+      console.log('idx', idx)
+      if (idx < 0) state.items.push(savedItem)
+      else state.items[idx] = savedItem
     },
-    actions: {
-        async loadItems({ commit }) {
-            try {
-                const items = await itemService.query()
-                commit({ type: 'setItems', items })
-                // socketService.off(SOCKET_EVENT_ITEM_ADDED)
-                // socketService.on(SOCKET_EVENT_ITEM_ADDED, item => {
-                //     console.log('Got item from socket', item)
-                //     commit({ type: 'additem', item })
-                // })
-                // socketService.off(SOCKET_EVENT_ITEM_ABOUT_YOU)
-                // socketService.on(SOCKET_EVENT_ITEM_ABOUT_YOU, item => {
-                //     console.log('item about me!', item)
-                // })
-            } catch (err) {
-                console.log('itemStore: Error in loadItems', err)
-                throw err
-            }
-        },
-        async removeItem({ commit }, { itemId }) {
-            try {
-                await itemService.remove(itemId)
-                commit({ type: 'removeItem', itemId })
-            } catch (err) {
-                console.log('itemStore: Error in removeItem', err)
-                throw err
-            }
-        },
-        async saveItem({ commit }, { item }) {
-            try {
-                const savedItem = await itemService.save(item)
-                commit({ type: 'updateItem', savedItem })
-            } catch (err) {
-                console.log('itemStore: Error in saveItem')
-                throw err
-            }
-        }
-    }
+    removeItem(state, { itemId }) {
+      state.items = state.items.filter(item => item._id !== itemId)
+    },
+  },
+  actions: {
+    async loadItems({ commit }) {
+      try {
+        const items = await itemService.query()
+        commit({ type: 'setItems', items })
+        // socketService.off(SOCKET_EVENT_ITEM_ADDED)
+        // socketService.on(SOCKET_EVENT_ITEM_ADDED, item => {
+        //     console.log('Got item from socket', item)
+        //     commit({ type: 'additem', item })
+        // })
+        // socketService.off(SOCKET_EVENT_ITEM_ABOUT_YOU)
+        // socketService.on(SOCKET_EVENT_ITEM_ABOUT_YOU, item => {
+        //     console.log('item about me!', item)
+        // })
+      } catch (err) {
+        console.log('itemStore: Error in loadItems', err)
+        throw err
+      }
+    },
+    async removeItem({ commit }, { itemId }) {
+      try {
+        await itemService.remove(itemId)
+        commit({ type: 'removeItem', itemId })
+      } catch (err) {
+        console.log('itemStore: Error in removeItem', err)
+        throw err
+      }
+    },
+    async saveItem({ commit }, { item }) {
+      try {
+        const savedItem = await itemService.save(item)
+        commit({ type: 'updateItem', savedItem })
+      } catch (err) {
+        console.log('itemStore: Error in saveItem')
+        throw err
+      }
+    },
+  },
 }
--- src/views/edit.vue
+++ src/views/edit.vue
@@ -1,48 +1,50 @@
 <template>
-    <main class="edit-item main-layout full">
-        <section class="edit-item__inner" v-if="item">
-            <back-btn>Go Back</back-btn>
-            <h2 class="edit-item-header">{{ editType }}</h2>
-            <item-edit-form :item="item" v-if="item" @saveItem="saveItem" />
-        </section>
-    </main>
+  <main class="edit-item main-layout full">
+    <section class="edit-item__inner" v-if="item">
+      <back-btn>Go Back</back-btn>
+      <h2 class="edit-item-header">{{ editType }}</h2>
+      <item-edit-form :item="item" v-if="item" @saveItem="saveItem" />
+    </section>
+  </main>
 </template>
 
 <script>
 import { showErrorMsg } from '../services/event-bus.service'
-import { itemService } from '../services/item.service'
+import { itemService } from '../services/comment.service'
 import { utilService } from '../services/util.service'
 import itemEditForm from '../cmps/item-edit-form.vue'
 import backBtn from '../cmps/back-btn.vue'
 
 export default {
-    name: '',
-    data() {
-        return {
-            item: null
-        }
+  name: '',
+  data() {
+    return {
+      item: null,
+    }
+  },
+  methods: {
+    saveItem(item) {
+      const itemToSave = JSON.parse(JSON.stringify(item))
+      this.$store.dispatch({ type: 'saveItem', item: itemToSave })
     },
-    methods: {
-        saveItem(item) {
-            const itemToSave = JSON.parse(JSON.stringify(item))
-            this.$store.dispatch({ type: 'saveItem', item: itemToSave })
-        }
+  },
+  computed: {
+    editType() {
+      return this.item._id ? 'Edit' : 'Add new'
     },
-    computed: {
-        editType() {
-            return (this.item._id) ? 'Edit' : 'Add new'
-        }
-    },
-    async created() {
-        const { id } = this.$route.params
-        try { this.item = await itemService.getById(id) }
-        catch (err) { showErrorMsg('Item not found') }
+  },
+  async created() {
+    const { id } = this.$route.params
+    try {
+      this.item = await itemService.getById(id)
+    } catch (err) {
+      showErrorMsg('Item not found')
+    }
 
-        if (!this.item) this.item = utilService.getEmptyItem()
-    },
-    components: { itemEditForm, backBtn },
+    if (!this.item) this.item = utilService.getEmptyItem()
+  },
+  components: { itemEditForm, backBtn },
 }
 </script>
 
-<style>
-</style>
+<style></style>
