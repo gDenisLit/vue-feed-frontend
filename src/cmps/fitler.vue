a00421ae78c0e2b5415da6f05082f9de02431da1
Guy Kadosh <guyk50@gmail.com>
2022-08-10 14:01:55 +0300

    started filterBy

---
+++ src/cmps/fitler.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="text" />
+</template>
+<script>
+import { debounce } from 'lodash'
+export default {
+  name: 'filter',
+  data() {
+    return {
+      filterBy: {
+        txt: '',
+      },
+    }
+  },
+}
+</script>
