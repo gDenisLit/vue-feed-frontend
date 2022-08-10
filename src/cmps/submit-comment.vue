816628be5153e4c2f17c2ba0101e048fa283428b
Guy Kadosh <guyk50@gmail.com>
2022-08-10 13:35:43 +0300

    submit-comment fisnihed

--- src/cmps/submit-comment.vue
+++ src/cmps/submit-comment.vue
@@ -1,36 +1,40 @@
 <template>
-  <form class="item-edit flex flex-column">
-    <input type="email" v-model="itemToEdit.title" placeholder="Email" />
-    <textarea cols="30" rows="10" v-model="itemToEdit.body"></textarea>
+  <form @submit="submitComment" class="item-edit flex flex-column">
+    <input type="email" v-model="comment.email" placeholder="Email" />
+    <textarea
+      cols="30"
+      rows="10"
+      v-model="comment.txt"
+      placeholder="Message"
+    ></textarea>
 
-    <div>
-      <button @click.prevent="saveItem">Save</button>
-      <button @click.prevent="clearFields">Clear</button>
-    </div>
+    <button>Submit</button>
   </form>
 </template>
 
 <script>
 export default {
-  name: 'item-edit-form',
-  props: { item: Object },
   data() {
-    return { itemToEdit: null }
+    return {
+      comment: {
+        email: '',
+        txt: '',
+      },
+    }
   },
   methods: {
-    saveItem() {
-      this.$emit('saveItem', this.itemToEdit)
-      this.$router.push('/list')
+    async submitComment() {
+      const comment = JSON.parse(JSON.stringify(this.comment))
+      await this.$store.dispatch({
+        type: 'submitComment',
+        comment,
+      })
     },
     clearFields() {
-      this.itemToEdit.title = ''
-      this.itemToEdit.subtitle = ''
-      this.itemToEdit.body = ''
+      this.comment.email = ''
+      this.comment.txt = ''
     },
   },
-  created() {
-    this.itemToEdit = JSON.parse(JSON.stringify(this.item))
-  },
-  emits: ['saveItem'],
+  created() {},
 }
 </script>
