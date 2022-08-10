471079823e85be4cc0f2e750a22867ed1cc51aac
Denis Lit <denislitviak@gmail.com>
2022-08-10 15:48:00 +0300

    add styling to feed

--- src/cmps/comment-preview.vue
+++ src/cmps/comment-preview.vue
@@ -1,10 +1,11 @@
 <template>
     <section>
-        <li class="comment-preview" v-if="comment">
-            <h2>{{ comment.name }}</h2>
-            <p>{{ comment.txt }}</p>
-            <p>{{ comment.email }}</p>
+        <li class="comment-preview flex" v-if="comment">
             <img :src="'https://www.gravatar.com/avatar/' + comment.imgId" />
+            <div class="info-wrapper flex flex-column justify-center">
+                <h3>{{ comment.email }}</h3>
+                <p>{{ comment.txt }}</p>
+            </div>
         </li>
     </section>
 </template>
